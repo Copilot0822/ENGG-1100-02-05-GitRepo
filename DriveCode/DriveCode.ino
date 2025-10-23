09167007d55ac1e63cd5cd3da58754782df4c7a8
Copilot0822 <123857131+Copilot0822@users.noreply.github.com>
2025-10-23 14:26:36 -0400

    Add initial L298N motor driver class and usage example
    Introduces a basic L298N motor driver class with speed and braking control, and demonstrates its usage in the Arduino setup and loop functions.

---
+++ DriveCode/DriveCode.ino
@@ -0,0 +1,60 @@
+class L298N{
+  int ena;
+  int in1;
+  int in2;
+  bool braking;
+
+
+public:
+  L298N(int enPin, int in1Pin, int in2Pin, bool brake = true){
+    ena = enPin;
+    in1 = in1Pin;
+    in2 = in2Pin;
+    braking = brake;
+
+  }
+
+  void setSpeed(int speed = 0){
+    if(speed == 0 && braking){
+      digitalWrite(in1, HIGH);
+      digitalWrite(in1, HIGH);
+      digitalWrite(ena, LOW);
+    }
+    else if(speed == 0 && !braking){
+      digitalWrite(in1, LOW);
+      digitalWrite(in1, LOW);
+      digitalWrite(ena, LOW);
+    }
+    else if(speed > 0){
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+      analogWrite(ena, speed);
+    }
+    else if(speed < 0){
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, HIGH);
+      analogWrite(ena, speed*-1);
+    }
+  }
+};
+
+int ena = 3;
+int in1 = 5;
+int in2 = 6;
+
+
+L298N drive(ena, in1, in2, false);
+
+
+void setup() {
+  // L298N drive(ena, in1, in2, false);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+
+  drive.setSpeed(150);
+  // put your main code here, to run repeatedly:
+
+}
