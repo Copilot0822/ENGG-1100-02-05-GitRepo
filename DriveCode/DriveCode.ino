a2b0efff07ee6f52c8fde5b7cb37a726a0ad63eb
Copilot0822 <123857131+Copilot0822@users.noreply.github.com>
2025-10-25 12:57:45 -0400

    Refactor L298N motor driver class and cleanup code
    Refactored the L298N class to move member functions inline and added comments for clarity. Cleaned up the setup and loop functions, removed commented code, and improved pin assignment and object initialization for better readability.

--- DriveCode/DriveCode.ino
+++ DriveCode/DriveCode.ino
@@ -1,69 +1,49 @@
-class L298N{
+class L298N{ //custom L298N motor driver object. 
   int ena;
   int in1;
   int in2;
   bool braking;
+  public:
+    L298N(int enPin, int in1Pin, int in2Pin, bool brake = true){
+      ena = enPin;
+      in1 = in1Pin;
+      in2 = in2Pin;
+      braking = brake;
 
-
-public:
-  L298N(int enPin, int in1Pin, int in2Pin, bool brake = true){
-    ena = enPin;
-    in1 = in1Pin;
-    in2 = in2Pin;
-    braking = brake;
-
-  }
-
-  void setSpeed(int speed = 0){
-    if(speed == 0 && braking){
-      digitalWrite(in1, HIGH);
-      digitalWrite(in1, HIGH);
-      digitalWrite(ena, LOW);
     }
-    else if(speed == 0 && !braking){
-      digitalWrite(in1, LOW);
-      digitalWrite(in1, LOW);
-      digitalWrite(ena, LOW);
+    void setSpeed(int speed = 0){
+      if(speed == 0 && braking){
+        digitalWrite(in1, HIGH);
+        digitalWrite(in1, HIGH);
+        digitalWrite(ena, LOW);
+      }
+      else if(speed == 0 && !braking){
+        digitalWrite(in1, LOW);
+        digitalWrite(in1, LOW);
+        digitalWrite(ena, LOW);
+      }
+      else if(speed > 0){
+        digitalWrite(in1, HIGH);
+        digitalWrite(in2, LOW);
+        analogWrite(ena, speed);
+      }
+      else if(speed < 0){
+        digitalWrite(in1, LOW);
+        digitalWrite(in2, HIGH);
+        analogWrite(ena, speed*-1);
+      }
     }
-    else if(speed > 0){
-      digitalWrite(in1, HIGH);
-      digitalWrite(in2, LOW);
-      analogWrite(ena, speed);
-    }
-    else if(speed < 0){
-      digitalWrite(in1, LOW);
-      digitalWrite(in2, HIGH);
-      analogWrite(ena, speed*-1);
-    }
-  }
 };
 
-
-//hello
+//pin assignment
 int ena = 3;
 int in1 = 5;
 int in2 = 6;
-
-
-L298N drive(ena, in1, in2, false);
-
-
-void setup() {
-  // L298N drive(ena, in1, in2, false);
-  // put your setup code here, to run once:
-
-}
-
+L298N drive(ena, in1, in2, false);//object init, (enablePin, in1, in2, brakingMode)
+void setup() {}
 void loop() {
-  // delay(1000);
-  drive.setSpeed(150);
-  delay(10000);
-  drive.setSpeed(-150);
-  delay(10000);
-  // digitalWrite(13, HIGH);
-  // delay(1000);
-  // drive.setSpeed(-150);
-  // digitalWrite(13, LOW);
-  // put your main code here, to run repeatedly:
-
+  drive.setSpeed(150);//set speed to go forward, (speed), speed>0 for forward, speed<0 for backwards
+  delay(10000);// 10s delay
+  drive.setSpeed(-150);//backwards command
+  delay(10000);//10s delay
 }
